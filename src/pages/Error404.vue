b1e284b506fb0bfc92d0bf0a751e44d52d48d5db
ttop5 <ttop5@qq.com>
2019-11-08 15:06:10 +0800

    perf: 通知和颜色调整

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -3,12 +3,12 @@
     <p>
       <img
         src="~assets/sad.svg"
-        style="width:30vw;max-width:150px;"
+        style="width:30vw; max-width:150px;"
       >
     </p>
     <p class="text-faded">Sorry, nothing here...<strong>(404)</strong></p>
     <q-btn
-      color="secondary"
+      color="primary"
       style="width:200px;"
       @click="$router.push('/')"
     >Go back</q-btn>
